8117d412cb2da6e452d3ff86dcda15b56452ad7e
Lisandro Molina <lisandromolina@MacBook-Pro.local>
2020-06-05 06:02:56 +0800

    Updates

---
+++ precache-manifest.5ba87d993634acb42934183536246575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec3475c6c043f2d501733c880093434",
+    "url": "/countdowntimer/index.html"
+  },
+  {
+    "revision": "1f3218cdf56fe4db3425",
+    "url": "/countdowntimer/static/css/main.a4607a92.chunk.css"
+  },
+  {
+    "revision": "9f2a8ef5dd7a43c0713e",
+    "url": "/countdowntimer/static/js/2.91dc4421.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdowntimer/static/js/2.91dc4421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3218cdf56fe4db3425",
+    "url": "/countdowntimer/static/js/main.4355e3b5.chunk.js"
+  },
+  {
+    "revision": "a89058c3fa3e3ee15804",
+    "url": "/countdowntimer/static/js/runtime-main.d39fb4a3.js"
+  }
+]);
