0ad3b01111d38c6e24fb0f8935b6fff949e603f9
Lisandro Molina <lisandromolina@MacBook-Pro.local>
2020-06-06 15:59:26 +0800

    Added documentation

--- src/App.js
+++ src/App.js
@@ -19,48 +19,54 @@ class App extends React.Component {
       speed: 1000
     }
   }
-
+  /**
+   * @description   Handles the update for the timer in every interval.
+   *                The interval speed is handled by state.speed
+   */
   tick = () => {
     //Check the timer if it has not reach 0 and if it is currently running
     if(this.state.timer > 0 && this.state.isRunning) {
-
       //Decrement the timer by 1 second and save new timer to the state
       let newTime = this.state.timer - 1;
       this.setState({
         timer: newTime,
         minutes: Math.floor(newTime / 60),
         seconds: newTime % 60,
-
       });
-
       //Disable Pause button if timer reaches 0
       if(this.state.timer === 0)
       this.setState({
         togglePauseResumeDisabled: true,
       });
     } else {
-
-      //if timer reaches 0, pause the timer
+      //if timer reaches 0, stop the timer
       if(this.state.timer === 0) {
         clearInterval(this.timerId);
         this.setState({
           isRunning: false,
         });
       }
-    }
-    
+    }  
   }
 
-  // Method for formatting the timer for display
+  /**
+   * @description   Formats the timer for display
+   * @return        Timer in MM:SS format enclosed in a span tag
+   */
   formatTime = (minutes, seconds) => {
-    if(minutes< 10)
-      minutes = "0" + minutes
-    if(seconds< 10)
-      seconds = "0" + seconds
+    if(minutes < 10)
+      minutes = "0" + minutes;
+    if(seconds < 10)
+      seconds = "0" + seconds;
     return (<span>{minutes}:{seconds}</span>);
   }
 
+  /**
+   * @description   Handles the event when input form is submitted.
+   * @param         {object} e Event object from the form that contains the initial timer value in minutes.
+   */
   onSubmit = (e) => {
+    //Prevents the page from refreshing.
     e.preventDefault();
     //Dont do anything is input is blank or zero
     if(e.target.startTime.value === "" || e.target.startTime.value === "0")
@@ -76,15 +82,14 @@ class App extends React.Component {
 
     //Save initial value to state
     this.setState({
-        timer: minutesToSeconds,
-        minutes: Math.floor(minutesToSeconds/60),
-        seconds: minutesToSeconds % 60,
-        initialValue: minutesToSeconds,
-        isRunning: true,
-        togglePauseResumeDisabled: false,
-        speed: 1000
-
-      });
+      timer: minutesToSeconds,
+      minutes: Math.floor(minutesToSeconds/60),
+      seconds: minutesToSeconds % 60,
+      initialValue: minutesToSeconds,
+      isRunning: true,
+      togglePauseResumeDisabled: false,
+      speed: 1000
+    });
 
     //Start the timer with initial speed 1000ms
     this.timerId = setInterval(
@@ -92,6 +97,9 @@ class App extends React.Component {
     )
   }
 
+  /**
+   * @description Method for Pause or Resume button is pressed.
+   */
   onTogglePauseResume = () => {
     let isRunning = this.state.isRunning;
     //Check if timer is running
@@ -109,10 +117,12 @@ class App extends React.Component {
     this.setState({
       isRunning: !isRunning
     });
-  
-    
   }
 
+  /**
+   * @description   Method for changing the speed of the timer interval
+   * @param         {Object} e Event object from the buttons that contains the value for speed interval in milliseconds 
+   */
   speedChange = (e) => {
     //Pause the timer before changing the speed
     clearInterval(this.timerId);
@@ -129,9 +139,13 @@ class App extends React.Component {
         () => this.tick(), e.target.value
       )
     } 
-
   }
 
+  /**
+   * @description   Method to check when timer is less than 20 and less than 10 
+   * @return        CSS class name for styling the timer when it is  or less 20 seconds or less and 10 seconds.    
+   *             
+   */
   getTimerStyle = () => {
     if(this.state.timer <= 20 && this.state.initialValue != null) {
       if(this.state.timer <= 10 && this.state.initialValue != null )
